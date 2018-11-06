4fa69bb5b85d77694210ccbed2115bd18f8f40f9
Adrian <Adrian.ca1971@gmail.com>
2018-11-06 23:18:56 +0100

    CMS - MENU

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrian
+ * Date: 06.11.2018
+ * Time: 21:42
+ */
+
+namespace App\Helpers;
+
+class Helpers {
+
+    public static function menu($slug){
+
+        return "siema";
+    }
+}
